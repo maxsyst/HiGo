36d9851f54e56d50dcfe1a15ec9a871e801be209
maxDevSvr117 <ubtentry@DevSvr117.com>
2024-08-27 23:22:23 +0800

    1

--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		data := map[string]interface{}{
-			"name":  "GoLang",
-			"value": "垒好",
-		}
-		//Output Json
-		c.JSON(http.StatusOK, data)
+	// 创建一个默认的 Gin 引擎
+	router := gin.Default()
+
+	// 定义一个 GET 请求的路由，路径为 /hello
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
 	})
-	r.Run(":9000")
+
+	// 启动服务器，监听在 8080 端口
+	router.Run(":9001")
 }
